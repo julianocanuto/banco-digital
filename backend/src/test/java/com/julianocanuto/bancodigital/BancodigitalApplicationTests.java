ac9eb033d14742b1bffda6be4f52c858f6359899
Juliano Canuto <julianocanuto@gmail.com>
2022-01-04 17:19:15 -0300

    Cria estrutura do backend

---
+++ backend/src/test/java/com/julianocanuto/bancodigital/BancodigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julianocanuto.bancodigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancodigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
