710facbb96afeb5297d777f00e3537512204174c
Juliano Canuto <julianocanuto@gmail.com>
2022-01-04 17:49:32 -0300

    Define a interface IConta

---
+++ backend/src/main/java/com/julianocanuto/bancodigital/entities/interfaces/IConta.java
@@ -0,0 +1,12 @@
+package com.julianocanuto.bancodigital.entities.interfaces;
+
+public interface IConta {
+
+	void sacar(double valor);
+
+	void depositar(double valor);
+
+	void transferir(double valor, IConta contaDestino);
+
+	void imprimirExtrato();
+}
