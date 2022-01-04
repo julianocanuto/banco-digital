3a441f79a759fff65442d00d3cf5f267bbb3560b
Juliano Canuto <julianocanuto@gmail.com>
2022-01-04 17:58:08 -0300

    Cria a entidade ContaCorrente

---
+++ backend/src/main/java/com/julianocanuto/bancodigital/entities/ContaCorrente.java
@@ -0,0 +1,14 @@
+package com.julianocanuto.bancodigital.entities;
+
+public class ContaCorrente extends Conta {
+
+	public ContaCorrente(Cliente cliente) {
+		super(cliente);
+	}
+
+	@Override
+	public void imprimirExtrato() {
+		System.out.println("=== Extrato Conta Corrente ===");
+		super.imprimirInfosComuns();
+	}
+}
